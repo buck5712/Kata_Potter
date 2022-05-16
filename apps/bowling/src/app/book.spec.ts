b32e3c3111264a8d9332b34015e07e4ace0561ae
buck5712 <buck5712@gmail.com>
2022-05-16 16:22:27 +0800

    RedLight1

--- apps/bowling/src/app/book.spec.ts
+++ apps/bowling/src/app/book.spec.ts
@@ -1,7 +1,34 @@
-import { Book } from './book';
-
-describe('Book', () => {
+import { Books } from './book';
+describe('testBasics', () => {
+  let books: Books;
+  beforeEach(() => {
+    books = new Books();
+  });
   it('should create an instance', () => {
-    expect(new Book()).toBeTruthy();
+    expect(books).toBeTruthy();
+  });
+  test('empty', () => {
+    //books.buy(1, 1);
+    expect(books.total_price).toBe(0);
+  });
+  test('buy 1', () => {
+    books.buy(1, 1);
+    expect(books.total_price).toBe(8);
+  });
+  test('buy 2', () => {
+    books.buy(2, 1);
+    expect(books.total_price).toBe(8);
+  });
+  test('buy 3', () => {
+    books.buy(3, 1);
+    expect(books.total_price).toBe(8);
+  });
+  test('buy 4', () => {
+    books.buy(4, 1);
+    expect(books.total_price).toBe(8);
+  });
+  test('buy 1*3', () => {
+    books.buy(1, 3);
+    expect(books.total_price).toBe(24);
   });
 });
